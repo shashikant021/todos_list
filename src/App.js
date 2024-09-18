1f0bb314671ebb3247e9b5aeaf8ecd2b5bc6d211
shashikant021 <shashikantsahu487@gmail.com>
2024-09-18 16:33:22 +0530

    half task done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header'
+import Footer from './MyComponents/Footer';
+import Todos from './MyComponents/Todos';
+import { useState } from 'react';
 
 function App() {
+  const onDelete = (todo) => {
+    console.log("I am OnDelete todo", todo);
+
+    // this method is not use in react to delete
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+  }
+
+  const [todos, setTodos] = useState([
+    {
+      sno: 1,
+      title: "list of rasan",
+      desc: "ghar "
+    },
+    {
+      sno: 1,
+      title: "list of rasan2",
+      desc: "ghar 2"
+    },
+    {
+      sno: 3,
+      title: "list of rasan3",
+      desc: "ghar1 "
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Header title="MyTodosList" />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+
+    </>
   );
 }
 
